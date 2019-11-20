fe816540cf098d065aaca2996db5f33db5501bcb
LobachAlexander <55191144+LobachAlexander@users.noreply.github.com>
2019-11-20 21:41:48 +0300

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -5,6 +5,7 @@ import numpy as num
 from sympy import *
 from sympy import I
 from numpy import arange
+import math
 
 
 # numZ = [0, 100]
@@ -34,40 +35,54 @@ def Lab2(numZ, denZ, numR, denR, n):
     plt.grid(True)
     plt.show()
 
-#    x = symbols("x")  # Определение полюсов передаточной функции
-#    Equation = solve(Eq(160 * x ** 3 + 89 * x ** 2 + 16.4 * x + 2))
-    # realX = []
-    # for i in Equation:
-    #    print(type(i))
-    #    if type(i) is sympy.core.numbers.Float:
-    #        a = i.real
-    #        realX.append(a)
-    #    else:
-    #        realX.append(i)
-#    print("Корни уравнения - ", Equation)
-
     G = control.tf(numR, denR)  # Найквист
-    GG = num.logspace(-3, 3, 5000)
-    control.nyquist(G, GG)
+    GG = num.logspace(-3, 3, 10000)
+    real, imag, freq = control.nyquist(G, GG)
+    real = list(real)
+    imag = list(imag)
     y = []
     x = []
     for i in range(-1, 2, 1):
         y.append(i)
     for i in range(-1, 2, 1):
         x.append(-1)
+    plt.plot(x, y, "r")
+
+    for i in range(0, len(real)):
+        z = abs(complex(real[i], imag[i]))
+        if 0.999 < z < 1.001 and real[i] < 0 and imag[i] < 0:
+            fi = atan(imag[i] / real[i]) * 180 / math.pi
+            print("Запас по фазе - ", real[i], imag[i])
+            plt.plot(real[i], imag[i], '-x')
+            print("Угол - ", fi)
+            plt.plot()
+            xx, yy = [0, real[i]], [0, imag[i]]
+            plt.plot(xx, yy)
+            break
+
+    for i in range(0, len(real)):
+        z = abs(complex(real[i], imag[i]))
+        if -0.0001 < imag[i] < 0.0001 and abs(real[i]) > 0.05:
+            print("Запас по амплитуде - ", real[i], imag[i])
+            plt.plot(real[i], imag[i], '-o')
+            xx1, yy1 = [-1, -1], [0, -1.1]
+            xx2, yy2 = [real[i], real[i]], [0, -1.1]
+            plt.plot(xx1, yy1, "r", xx2, yy2, "r")
+            break
+
     phi = num.linspace(0, 2 * num.pi, 100)
     r = num.sqrt(1.0)
     x1 = r * num.cos(phi)
     x2 = r * num.sin(phi)
     plt.plot(x1, x2)
     plt.title("Диаграмма Найквиста")
-    plt.plot(x, y, "r", label="Название")
-    plt.xlabel("+j")
-    plt.ylabel("+1")
+    plt.plot(label="Название")
+    plt.xlabel("+1")
+    plt.ylabel("+j")
     plt.grid(True)
     plt.show()
 
-    mag, phase, omega = bode(w2, dB=False)  # ЛАЧХ и ЛФЧХ
+    mag, phase, omega = bode(w1, dB=True)  # ЛАЧХ и ЛФЧХ
     plt.title("ЛАЧХ и ЛФЧХ")
     plt.grid(True)
     plt.show()
@@ -97,7 +112,12 @@ def Lab2(numZ, denZ, numR, denR, n):
     plt.grid(True)
     plt.show()
 
-    Matrix = num.matrix([[89, 9.12249], [160, 16.4]])
+    if n == 1:
+        Matrix = num.matrix([[89, 2], [160, 16.4]])
+    elif n == 2:
+        Matrix = num.matrix([[89, 9.1225], [160, 16.4]])
+    elif n == 3:
+        Matrix = num.matrix([[89, 10], [160, 16.4]])
     Det = num.linalg.det(Matrix)
     print("Уравнение - ", denZ, "\nМатрица - \n", Matrix)
     print(" Определитель - ", Det)
@@ -105,6 +125,6 @@ def Lab2(numZ, denZ, numR, denR, n):
 
 # Lab2([0, 100], [160, 89, 16.4, 2], [0, 1], [160, 89, 16.4, 1], 1)  # Начальная функция
 
-Lab2([0, 100], [160, 89, 16.4, 9.12249], [0, 4], [160, 89, 16.4, 1], 2)   # Функция при Кос = Кос.пред.
+# Lab2([0, 100], [160, 89, 16.4, 9.12245], [0, 8.12245], [160, 89, 16.4, 1], 2)   # Функция при Кос = Кос.пред.
 
-# Lab2([0, 100], [160, 89, 16.4, 10], [0, 10], [160, 89, 16.4, 1], 3)   # Функция при Кос > Кос.пред.
+# Lab2([0, 100], [160, 89, 16.4, 10], [0, 9], [160, 89, 16.4, 1], 3)   # Функция при Кос > Кос.пред.
